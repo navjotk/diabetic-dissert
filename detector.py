ca8733d9246a103a6fd90a5b792a8d61fb1104ec
Navjot Kukreja <navjot@gharpay.in>
2015-08-04 17:46:56 -0300

    detect output to file so progressbar doesn't break

--- detector.py
+++ detector.py
@@ -4,8 +4,9 @@ Created on 4 Aug 2015
 @author: navjotkukreja
 '''
 import platform
+import sys
 
-if platform.system()=='Windows':
+if platform.system()=='Windows' or not sys.stdout.isatty():
     GLOBAL_WINDOWS = True
 else:
     GLOBAL_WINDOWS = False
